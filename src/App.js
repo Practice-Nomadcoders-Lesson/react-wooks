f68755e81d07952881bd4c0b64a02527e6412b4b
iinsue <hinsu93@naver.com>
2022-04-11 08:26:30 +0900

    #1.2 useInput

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { useState } from "react";
+import { useInput } from "./useInpust";
 
 function App() {
-  const [item, setItem] = useState(0);
-  const incrementItem = () => {
-    setItem((item) => item + 1);
-  };
-  const decrementItem = () => {
-    setItem((item) => item - 1);
-  };
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr. ", maxLen);
   return (
-    <div>
-      <h1>Hello {item}</h1>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
