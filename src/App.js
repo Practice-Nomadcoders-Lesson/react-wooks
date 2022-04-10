fb7ceb059345e0de786419fa09373b17e20d5487
iinsue <hinsu93@naver.com>
2022-04-11 07:45:45 +0900

    #1.0 Inroduction to useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const [item, setItem] = useState(0);
+  const incrementItem = () => {
+    setItem((item) => item + 1);
+  };
+  const decrementItem = () => {
+    setItem((item) => item - 1);
+  };
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
 }
