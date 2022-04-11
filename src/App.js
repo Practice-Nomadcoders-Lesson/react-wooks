adb227d34084770360e973f4f081f8e7f92d236b
iinsue <hinsu93@naver.com>
2022-04-11 16:57:12 +0900

    #2.6 useFullscreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { useScroll } from "./useScroll/useScroll";
+import { useFullscreen } from "./useFullscreen/useFullscreen";
 
 function App() {
-  const { y } = useScroll();
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
-        Hello
-      </h1>
+      <div ref={element}>
+        <img
+          src="https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixlib=rb-1.2.1"
+          alt="grape"
+          width="550"
+        />
+        <button onClick={exitFull}>Exit Fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make Fullscreen</button>
     </div>
   );
 }
