daa3d75a5ffaad51601318b942decd5033ca6204
iinsue <hinsu93@naver.com>
2022-04-11 09:32:43 +0900

    1.3 useTabs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useInput } from "./useInpust";
+import { useTabs } from "./useTabs";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Sections 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Sections 2",
+  },
+];
 
 function App() {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr. ", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button key={index} onClick={() => changeItem(index)}>
+          {section.tab}
+        </button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
---
+++ src/useTabs.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const useTabs = (initalTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initalTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
