e4aa0968ea9633e61d488ac446ea29043434eaa0
iinsue <hinsu93@naver.com>
2022-04-11 11:32:48 +0900

    #2.2 useClick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { useTitle } from "./useTitle";
+import { useRef, useEffect } from "react";
+
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+
+  if (typeof onClick !== "function") {
+    return;
+  }
+  return element;
+};
 
 function App() {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
+  const sayHello = () => console.log("say Hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
